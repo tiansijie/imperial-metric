c6e00ba97ae40f08f39d7e75ab540cb2adf0b426
Sijie Tian <sijie@socialtables.com>
2016-03-15 22:15:36 -0400

    add new method for transformation

--- index.js
+++ index.js
@@ -1,66 +1,55 @@
 
 var ImperialMetric = function(val) {
 	this.val = val;
+	this.map = {
+		inch: {
+			belong: "imperial",
+			toMain: 1,
+			toOthers: (2.54 / 100)
+		},
+		foot: {
+			belong: "imperial",
+			toMain: 12,
+			toOthers: (2.54 / 100)
+		},
+		m: {
+			belong: "metric",
+			toMain: 1,
+			toOthers: (1 / 2.54) * 100
+		},
+		cm: {
+			belong: "metric",
+			toMain: 1 / 100,
+			toOthers: (1 / 2.54) * 100
+		},
+		mm: {
+			belong: "metric",
+			toMain: 1 / 1000,
+			toOthers: (1 / 2.54) * 100
+		},
+		km: {
+			belong: "metric",
+			toMain: 1000,
+			toOthers: (1 / 2.54) * 100
+		}
+	}
 }
 
 /*
 	"inch", "cm", "m", "foot"
 */
 ImperialMetric.prototype.from = function(unit) {
-	this.unit = unit;
+	this.unit = this.map[unit];
 	return this;
 }
 
-
-ImperialMetric.prototype.toInch = function() {
-	switch(this.unit) {
-		case "inch":
-			return this.val;
-		case "cm":
-			return this.val * (1 / 2.54);
-		case "m":
-			return this.val * (1 / 2.54) * 100;
-		case "foot":
-			return this.val * 12;
+ImperialMetric.prototype.to = function(goal) {
+	this.goal = this.map[goal];
+	if (this.unit.belong !== this.goal.belong) {
+		return this.val * this.unit.toMain * this.unit.toOthers / this.goal.toMain;
 	}
-}
-
-ImperialMetric.prototype.toFoot = function() {
-	switch(this.unit) {
-		case "inch":
-			return this.val * (1 / 12);
-		case "cm":
-			return this.val * (1 / 30.48);
-		case "m":
-			return this.val * (1 / 30.48) * 100;
-		case "foot":
-			return this.val;
-	}
-}
-
-ImperialMetric.prototype.toCm = function() {
-	switch(this.unit) {
-		case "inch":
-			return this.val * 2.54;
-		case "cm":
-			return this.val;
-		case "m":
-			return this.val * (1 / 100);
-		case "foot":
-			return this.val * 30.48;
-	}
-}
-
-ImperialMetric.prototype.toM = function() {
-	switch(this.unit) {
-		case "inch":
-			return this.val * (2.54 / 100);
-		case "cm":
-			return this.val * (1 / 100);
-		case "m":
-			return this.val;
-		case "foot":
-			return this.val * (30.48 / 100);
+	else {
+		return this.val * this.unit.toMain / this.goal.toMain;
 	}
 }
 
