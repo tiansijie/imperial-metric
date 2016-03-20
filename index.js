24ab3a257e8ad79f20a3c7e88d0a7d7e90fff9e9
Sijie Tian <sijie@socialtables.com>
2016-03-20 17:41:27 -0400

    added sqrt things

--- index.js
+++ index.js
@@ -36,39 +36,39 @@ var ImperialMetric = function(val) {
 		},
 		"sqrt-foot": {
 			belong: IMPERIAL,
-			toMain: 1,
-			toOther: 1,
+			toMain: 144,
+			toOthers: (6.4516 / 10000),
 		},
 		"sqrt-inch": {
 			belong: IMPERIAL,
 			toMain: 1,
-			toOther: 1
+			toOthers: (6.4516 / 10000)
 		},
 		"sqrt-m": {
 			belong: MERTRIC,
 			toMain: 1,
-			toOther: 1
+			toOthers: (1 / 6.4516) * 10000
 		},
 		"sqrt-cm": {
 			belong: MERTRIC,
-			toMain: 1,
-			toOther: 1
+			toMain: 1 / 10000,
+			toOthers: (1 / 6.4516) * 10000
 		},
 		"sqrt-mm": {
 			belong: MERTRIC,
-			toMain: 1,
-			toOther: 1
+			toMain: 1 / 1000000,
+			toOthers: (1 / 6.4516) * 10000
 		},
 		"sqrt-km": {
 			belong: MERTRIC,
-			toMain: 1,
-			toOther: 1
+			toMain: 1000000,
+			toOthers: (1 / 6.4516) * 10000
 		}
 	}
 }
 
 /*
-	"inch", "cm", "m", "foot"
+	"inch", "cm", "m", "foot", "sqrt-m", "sqrt-km", "sqrt-cm", "sqrt-mm", "sqrt-inch", "sqrt-foot"
 */
 ImperialMetric.prototype.from = function(unit) {
 	this.unit = this.map[unit];
